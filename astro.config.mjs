e837c1646050a6899e71eddf9cde784196cd4120
Michael Jolley <mike@sparcapp.io>
2023-08-20 00:19:08 -0500

    Working on styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,4 @@
 import { defineConfig } from 'astro/config';
-import compress from 'astro-compress';
-import sitemap from '@astrojs/sitemap';
-
-import preact from '@astrojs/preact';
-
-import codeTheme from './code-theme.json';
 
 // https://astro.build/config
-export default defineConfig({
-  site: `https://baldbeardedbuilder.com`,
-  output: 'static',
-  trailingSlash: 'always',
-  integrations: [
-    sitemap({
-      filter: (page) =>
-        page !== 'https://baldbeardedbuilder.com/thanks/' &&
-        page !== 'https://baldbeardedbuilder.com/404/',
-    }),
-    preact(),
-    compress({
-      logger: 0,
-    }),
-  ],
-  markdown: {
-    shikiConfig: {
-      theme: codeTheme,
-    },
-  },
-});
+export default defineConfig({});
