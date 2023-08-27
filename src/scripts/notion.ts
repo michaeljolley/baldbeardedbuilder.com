ed8f5770c74e714eb305e8445d5387ee6dc593ed
Michael Jolley <mike@sparcapp.io>
2023-08-26 21:43:20 -0500

    Finishing home

--- src/scripts/notion.ts
+++ src/scripts/notion.ts
@@ -1,4 +1,5 @@
 import { Client } from '@notionhq/client';
+
 import type { ScheduledEvent } from '@type/scheduledEvent';
 
 export async function getEvents(): Promise<ScheduledEvent[]> {
@@ -6,24 +7,30 @@ export async function getEvents(): Promise<ScheduledEvent[]> {
 
   const pages = await notion.databases.query({
     database_id: import.meta.env.NOTION_CONTENT_DATABASE_ID,
+
     filter: {
       and: [
         {
           property: 'Status',
+
           status: {
             equals: '📆 Staged',
           },
         },
+
         {
           or: [
             {
               property: 'Type',
+
               select: {
                 equals: 'Long Form Video',
               },
             },
+
             {
               property: 'Type',
+
               select: {
                 equals: 'Livestream',
               },
@@ -34,22 +41,27 @@ export async function getEvents(): Promise<ScheduledEvent[]> {
     },
   });
 
-  console.dir(pages, { depth: null });
-
   const events = pages.results
+
     .map((page) => {
       return {
         id: page.id,
+
         title: page.properties.Name.title[0].plain_text,
+
         description: page.properties.Description.rich_text[0].plain_text,
+
         date: new Date(page.properties['Release Date'].date.start),
+
         type:
           page.properties.Type.select.name === 'Long Form Video'
             ? 'YouTube'
             : 'Twitch',
       };
     })
+
     .sort((a, b) => a.date.getTime() - b.date.getTime())
+
     .splice(0, 5);
 
   return events;
