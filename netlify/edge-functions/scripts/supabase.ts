81483e332672f4550e74d87082b12fa3d159c999
Michael <mike@sparcapp.io>
2023-10-08 22:49:22 -0500

    Working on edge functions

--- netlify/edge-functions/scripts/supabase.ts
+++ netlify/edge-functions/scripts/supabase.ts
@@ -1,12 +1,9 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 import type { Analytic } from '../types/analytics.ts';
 
-const SUPABASE_URL = Deno.env.get('SUPABASE_URL') || process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = Deno.env.get('SUPABASE_ANON_KEY') || process.env.SUPABASE_ANON_KEY;
-
 const supabase = createClient(
-  SUPABASE_URL as string,
-  SUPABASE_ANON_KEY as string,
+  Deno.env.get('SUPABASE_URL') as string,
+  Deno.env.get('SUPABASE_ANON_KEY') as string,
   {
     auth: {
       persistSession: false,
