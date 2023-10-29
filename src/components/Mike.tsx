78b292a7c46b5f01be17fcef61cc5aa402cb99b1
Michael Jolley <mike@sparcapp.io>
2023-10-28 21:06:01 -0500

    Finished update

--- src/components/Mike.tsx
+++ src/components/Mike.tsx
@@ -1,9 +1,11 @@
+import "./Mike.css";
+
 export default function () {
   const imageNumber = Math.floor(Math.random() * (14 - 1) + 1);
 
   return (
     <img
-      style="width:100%;mix-blend-mode:overlay;"
+      style="width:100%;mix-blend-mode:luminosity;"
       src={`https://res.cloudinary.com/dk3rdh3yo/image/upload/f_auto/v1607302424/mj_${imageNumber}.png`}
       alt="Image of Michael making an odd face/pose."
     />
