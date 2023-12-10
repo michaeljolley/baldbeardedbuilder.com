1b6d5c54cb22fd33832fba4d73b2b7c1c5c7df53
Michael Jolley <mike@sparcapp.io>
2023-12-09 21:33:38 -0600

    Finishing changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://baldbeardedbuilder.com",
 	integrations: [sitemap(), preact()],
 });
