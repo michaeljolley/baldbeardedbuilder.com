cda9e54c775f69f5c2afeee0c5505e052f1d32a8
Michael Jolley <mike@sparcapp.io>
2023-12-12 18:24:54 -0600

    Fixing RSS & copylink cleanup

--- src/scripts/copyLink.ts
+++ src/scripts/copyLink.ts
@@ -1,21 +1,16 @@
-import tippy from 'tippy.js';
+import tippy from "tippy.js";
 
-function copyLink(el: HTMLAnchorElement) {
-	const id = el.href;
-	const url = window.location.href.replace(window.location.hash, '');
-	navigator.clipboard.writeText(el.href);
-}
+function copyLink(el: HTMLAnchorElement) {}
 
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
 	document.querySelectorAll("a.autolink-header").forEach((a) => {
 		a.addEventListener("click", (e: Event) => {
 			e.preventDefault();
-			copyLink(a as HTMLAnchorElement);
-
+			navigator.clipboard.writeText((a as HTMLAnchorElement).href);
 			const t = tippy(a, {
 				arrow: true,
 				placement: "top",
-				content: "Copied"
+				content: "Copied",
 			});
 			t.show();
 			setTimeout(() => {
