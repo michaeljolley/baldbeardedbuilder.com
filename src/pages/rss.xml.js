196f38d195975abb95f6ba7561ac418e45680280
Michael Jolley <mike@sparcapp.io>
2023-12-20 22:45:52 -0600

    RSS fixes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -13,54 +13,54 @@ export async function GET() {
 	const posts = [];
 	for (const post of blogPosts) {
 		const { Content } = await post.render();
-		posts.push(`<entry>
-		<id>https://baldbeardedbuilder.com/blog/${post.slug}/</id>
+		posts.push(`<item>
+		<author>mike@baldbeardedbuilder.com (Michael Jolley)</author>
 		<title><![CDATA[${post.data.title}]]></title>
-		<link href="https://baldbeardedbuilder.com/blog/${post.slug}/"/>
-		<updated>${post.data.pubDate.toISOString()}</updated>
-		${post.data.tags.map((tag) => `		<category term="${tag}"/>`).join("\n")}
-		<summary><![CDATA[${post.data.description}]]></summary>
-	</entry>`);
+		<pubDate>${post.data.pubDate.toUTCString()}</pubDate>
+		<guid>https://baldbeardedbuilder.com/blog/${post.slug}/</guid>
+		<link>https://baldbeardedbuilder.com/blog/${post.slug}/</link>
+		<description><![CDATA[${post.data.description}]]></description>
+	${post.data.tags.map((tag) => `	<category>${tag}</category>`).join("\n")}		
+	</item>`);
 	}
 	const xmlPosts = posts.join("\n");
 
 	let dumps = [];
 	for (const post of brainDumps) {
 		const { Content } = await post.render();
-		dumps.push(`<entry>
-		<id>https://baldbeardedbuilder.com/brain-dump/${post.slug}/</id>
-		<title><![CDATA[${post.data.title}]]></title>
-		<link href="https://baldbeardedbuilder.com/brain-dump/${post.slug}/"/>
-		<updated>${post.data.pubDate.toISOString()}</updated>
-${post.data.tags.map((tag) => `		<category term="${tag}"/>`).join("\n")}
-		<summary><![CDATA[${post.data.description}</summary>
-	</entry>`);
+		dumps.push(`<item>
+	<author>mike@baldbeardedbuilder.com (Michael Jolley)</author>
+	<title><![CDATA[${post.data.title}]]></title>
+	<pubDate>${post.data.pubDate.toUTCString()}</pubDate>
+	<guid>https://baldbeardedbuilder.com/brain-dump/${post.slug}/</guid>
+	<link>https://baldbeardedbuilder.com/brain-dump/${post.slug}/</link>
+	<description><![CDATA[${post.data.description}]]></description>
+${post.data.tags.map((tag) => `	<category>${tag}</category>`).join("\n")}		
+</item>`);
 	}
 
 	const xmlBrainDumps = dumps.join("\n");
 
 	const rss = `<?xml version="1.0" encoding="utf-8"?>
 	<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-		
-		<atom:link href="https://baldbeardedbuilder.com/rss.xml" rel="self" type="application/rss+xml" />
+		<channel>
+			<atom:link href="https://baldbeardedbuilder.com/rss.xml" rel="self" type="application/rss+xml" />
+			<description>Articles, videos, and more from Michael Jolley, the Bald Bearded Builder.</description>
+			<link>https://baldbeardedbuilder.com/</link>
+			<title>Michael Jolley is the Bald Bearded Builder</title>
+			<category>dotnet</category>
+			<category>javascript</category>
+			<category>csharp</category>
+			<category>typescript</category>
+			<category>programming</category>
+			<language>en-us</language>
+			<lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+			<pubDate>${new Date().toUTCString()}</pubDate>
 
-	<title>Michael Jolley is the Bald Bearded Builder</title>
-	<link href="https://baldbeardedbuilder.com/"/>
-	<updated>${new Date().toISOString()}</updated>
-	<category term="dotnet"/>
-	<category term="javascript"/>
-	<category term="csharp"/>
-	<category term="typescript"/>
-	<category term="programming"/>
-	<id>https://baldbeardedbuilder.com/</id>
-	<author>
-		<name>Michael Jolley</name>
-		<email>mike@baldbeardedbuilder.com</email>
-		<uri>https://baldbeardedbuilder.com/</uri>
-	</author>
-	${xmlPosts}
-	${xmlBrainDumps}
-</rss>`;
+			${xmlPosts}
+			${xmlBrainDumps}
+		</channel>
+	</rss>`;
 
 	return new Response(rss, {
 		status: 200,
