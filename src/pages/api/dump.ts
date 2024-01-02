be7aeba300ec9ee25d4dff4ce14ef9c864b56143
Michael Jolley <mike@sparcapp.io>
2024-01-01 21:44:46 -0600

    Fixing Brain Dump form

--- src/pages/api/dump.ts
+++ src/pages/api/dump.ts
@@ -7,15 +7,17 @@ export const POST: APIRoute = async ({ request }) => {
 	const bot = data.get("bot-field");
 	const first_name = data.get("firstName");
 	const email = data.get("email");
+	const api_key = import.meta.env.CONVERTKIT_API_KEY;
 
-	if (!bot && first_name && email) {
+	if ((!bot || bot.toString().length > 0) && first_name && email) {
 
 		const body = {
+			api_key,
 			first_name,
 			email
 		}
 
-		await fetch("https://api.convertkit.com/v3/forms/5311675/subscribe", {
+		const response = await fetch("https://api.convertkit.com/v3/forms/5311675/subscribe", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
