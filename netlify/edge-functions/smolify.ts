fdc092297716545f952482707a9a77591f92f11d
Michael Jolley <mike@baldbeardedbuilder.com>
2024-02-21 15:06:37 -0600

    Starting up

---
+++ netlify/edge-functions/smolify.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'https://edge.netlify.com/';
+import { getShortUrl } from './scripts/supabase.ts';
+
+export default async function handler(req: Request) {
+  // Search for the placeholder
+  const url = new URL(req.url);
+  const slug = url.searchParams.get('path');
+
+  if (slug) {
+    try {
+      const target = await getShortUrl(slug);
+
+      if (target) {
+        return Response.redirect(target, 301);
+      } else {
+        return Response.redirect('http://baldbeardedbuilder.com/404/', 301);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return Response.redirect('http://baldbeardedbuilder.com/', 301);
+}
+
+export const config: Config = {
+  path: '/api/smolify',
+};
