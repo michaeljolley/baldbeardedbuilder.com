7197d7478206fd2fa1d59081d6cbcf9b9ed0bebe
Michael Jolley <mike@baldbeardedbuilder.com>
2024-02-22 19:41:57 -0600

    Adding supabase metrics

--- netlify/edge-functions/scripts/supabase.ts
+++ netlify/edge-functions/scripts/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import type { Analytic } from "netlify/edge-functions/types/analytic";
 
 const supabase = createClient(
   Netlify.env.get('SUPABASE_URL') as string,
@@ -9,6 +10,14 @@ const supabase = createClient(
     },
   }
 );
+
+export async function insertAnalytic(analytic: Analytic) {
+	const { error } = await supabase.from("analytics").insert(analytic);
+
+	if (error) {
+		console.error(error);
+	}
+}
 
 export async function getShortUrl(slug: string) {
   const { data, error } = await supabase
---
+++ netlify/edge-functions/types/analytic.ts
@@ -0,0 +1,12 @@
+export type Analytic = {
+	id?: string;
+	host: string;
+	timezone: string;
+	path: string;
+	xy: string;
+	session: string;
+	created_at?: string;
+	utm_source?: string;
+	utm_medium?: string;
+	utm_campaign?: string;
+};
