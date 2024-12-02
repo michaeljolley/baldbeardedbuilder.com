869c7293d869d0ce2bf9642940f9063a3e375348
Michael Jolley <mike@baldbeardedbuilder.com>
2024-12-01 18:56:15 -0600

    Finalizing gifting page

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -3,8 +3,6 @@ export const prerender = false;
 import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
 
-const host = import.meta.env.HOST;
-
 export const GET: APIRoute = async ({ request, cookies, redirect }) => {
   
 	const { data, error } = await supabase.auth.signInWithOAuth({
